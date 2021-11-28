82e768fd3b12aaf704ca68dee6360457d10638f8
João Pedro de Abreu Martins Pereira <jpampereira@gmail.com>
2021-11-28 05:22:09 -0300

    Alteração para permitir o envio de mais de um parâmetro

--- desenvolvimento/nginx/scripts/requests.js
+++ desenvolvimento/nginx/scripts/requests.js
@@ -1,9 +1,9 @@
 const url = 'http://localhost:1880';
 
-async function request(endpoint, param, method) {
+async function request(endpoint, params, method) {
 	if (method === 'get') {
 		try {
-			const res = await axios.get(url + endpoint, { params: { param } });
+			const res = await axios.get(url + endpoint, { params });
 			return res.data;
 		} catch (e) {
 			throw e;
