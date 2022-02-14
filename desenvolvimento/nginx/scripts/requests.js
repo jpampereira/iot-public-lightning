033a5ceafa68bd9dae0a1f073ede1c4e956e3c5a
João Pedro de Abreu Martins Pereira <jpampereira@gmail.com>
2022-02-14 20:37:53 -0300

    Alteração no código para permitir request HTTP em outros formatos além do tipo GET

--- desenvolvimento/nginx/scripts/requests.js
+++ desenvolvimento/nginx/scripts/requests.js
@@ -1,12 +1,10 @@
 const url = 'http://localhost:1880';
 
 async function request(endpoint, params, method) {
-	if (method === 'get') {
-		try {
-			const res = await axios.get(url + endpoint, { params });
-			return res.data;
-		} catch (e) {
-			throw e;
-		}
+	try {
+		const res = await axios[method](url + endpoint, { params });
+		return res.data;
+	} catch (e) {
+		throw e;
 	}
 }
