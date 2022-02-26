2cdc87dc2b48aa3b14906a2101b4c825b78aaaf5
João Pedro de Abreu Martins Pereira <jpampereira@gmail.com>
2022-02-26 05:25:13 -0300

    Otimização do botão para ligar/desligar o dispositivo + Correção no fluxo de geração de falhas

--- desenvolvimento/nginx/scripts/monitoring.js
+++ desenvolvimento/nginx/scripts/monitoring.js
@@ -118,6 +118,78 @@ function formSubmission(e) {
 	}
 }
 
+/************ INFO CARD FUNCTIONS *************/
+
+function updateInfoCard(infos) {
+	const labels = { street: 'ENDEREÇO', district: 'BAIRRO', zone: 'ZONA', status: 'STATUS' }
+
+	infos = Object.entries(infos[0]).filter(info => labels[info[0]] !== undefined); // Get only information that will be displayed
+	
+	const newValues = infos.map(info => {
+		let area = createElement('div');
+		const label = createElement('p');
+		const value = createElement('p');
+		
+		label.innerHTML = `${labels[info[0]]}:`;
+		value.innerHTML = info[1];
+		
+		if (info[0] === 'status') {			
+			value.classList.add(info[1].replace(/\s+/, '_'));
+		}
+		
+		appendChilds(area, [label]);
+		appendChilds(area, [value]);
+		
+		return area;
+	});
+
+	removeChilds(infoCard, window.document.querySelectorAll('#info > *'));
+	appendChilds(infoCard, newValues);
+}
+
+function resetInfoCard() {
+	removeChilds(infoCard, window.document.querySelectorAll('#info > *'));
+
+	const div = createElement('div');
+	
+	const img = createElement('img', { src: '../images/monitoring/not-found.png', style: 'margin-bottom: 20px;'});
+	appendChilds(div, [img]);
+
+	const p = createElement('p');
+	p.innerHTML = 'Dispositivo não encontrado';
+	appendChilds(div, [p]);
+	
+	appendChilds(infoCard, [div]);
+}
+
+/*********** ON/OFF BUTTON FUNCTIONS **********/
+
+function changeBtnLabel(status) {
+	const btnState = button.getAttribute('btn_state');
+	const btnLabels = ['Ligar', 'Desligar', 'Ligar/Desligar'];
+
+	if (status !== btnState) {
+		button.setAttribute('btn_state', status);
+		button.innerHTML = btnLabels[status];
+	}
+}
+
+function sendAction () {
+	const btnState = button.getAttribute('btn_state');
+
+	if (btnState !== '2') {
+		let action = 'D';
+
+		if (btnState === '0') {
+			action = 'L';
+		}
+
+		const device_name = form.elements.current_name.value;
+
+		request('/devices/actions/on-off', { device_name, action }, 'get');
+	}
+}
+
 /******************** MAIN ********************/
 
 window.onload = function () {
@@ -127,3 +199,8 @@ window.onload = function () {
 
 const form = window.document.forms[0];
 form.onsubmit = formSubmission;
+
+const infoCard = window.document.querySelector('#info');
+
+const button = window.document.getElementById('btn-on-off');
+button.onclick = sendAction;
